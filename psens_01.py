e6b88636f81dd510eb834331f9ad52c5ae8cd0ae
Melvin Gauci <mgauci@g.harvard.edu>
2018-09-25 14:21:54 -0400

    Update psens_01.py

--- psens_01.py
+++ psens_01.py
@@ -88,13 +88,13 @@ class DepthSensor():
 		# Read digital temperature value
 		# Read data back from 0x00(0), 3 bytes
 		# D2 MSB2, D2 MSB1, D2 LSB
-		value = bus.read_i2c_block_data(0x77, 0x00, 3)
+		value = bus.read_i2c_block_data(0x76, 0x00, 3)
 		D2 = value[0] * 65536 + value[1] * 256 + value[2]
 
 		dT = D2 - C5 * 256
 		TEMP = 2000 + dT * C6 / 8388608
-		OFF = C2 * 131072 + (C4 * dT) / 64
-		SENS = C1 * 65536 + (C3 * dT ) / 128
+		OFF = C2 * 65536 + (C4 * dT) / 128
+		SENS = C1 * 32768 + (C3 * dT ) / 256
 		T2 = 0
 		OFF2 = 0
 		SENS2 = 0
@@ -103,13 +103,14 @@ class DepthSensor():
 			T2 = 0
 			OFF2 = 0
 			SENS2 = 0
+			if TEMP > 4500 :
+				SENS2 = SENS2 - ((TEMP - 4500) * (TEMP - 4500)) / 8
 		elif TEMP < 2000 :
 			T2 = (dT * dT) / 2147483648
-			OFF2= 61 * ((TEMP - 2000) * (TEMP - 2000)) / 16
-			SENS2= 2 * ((TEMP - 2000) * (TEMP - 2000))
+			OFF2 = 3 * ((TEMP - 2000) * (TEMP - 2000))
+			SENS2 = 7 * ((TEMP - 2000) * (TEMP - 2000)) / 8
 			if TEMP < -1500 :
-				OFF2 = OFF2 + 20 * ((TEMP + 1500) * (TEMP + 1500))
-				SENS2 = SENS2 + 12 * ((TEMP + 1500) * (TEMP +1500))
+				SENS2 = SENS2 + 2 * ((TEMP + 1500) * (TEMP + 1500))
 
 		TEMP = TEMP - T2
 		OFF = OFF - OFF2
