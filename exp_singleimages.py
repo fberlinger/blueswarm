ee0470d9300c2ede9f75ab19a2aca8e916824468
Florian Berlinger <fberlinger@seas.harvard.edu>
2018-09-26 15:28:48 -0400

    flash leds before picture

--- exp_singleimages.py
+++ exp_singleimages.py
@@ -1,4 +1,5 @@
 from utils import *
+import time
 
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
@@ -8,11 +9,24 @@ GPIO.setup(CAMLED, GPIO.OUT)
 
 from picamera import PiCamera
 picam = PiCamera()
-picam.resolution = (640, 480)
+picam.resolution = (2592, 1944)
+picam.framerate = 15
 picam.rotation = 180
 
-GPIO.output(CAMLED, False)
-picam.capture('{}_sr.jpg'.format(U_FILENAME), use_video_port=True)
+# light led to ready people for photo
+GPIO.setup(13, GPIO.OUT)
+GPIO.output(13, GPIO.HIGH)
+time.sleep(4)
+GPIO.output(13, GPIO.LOW)
 
+# take photos
+GPIO.output(CAMLED, False)
+picam.capture('{}_sr.jpg'.format(U_FILENAME))
 GPIO.output(CAMLED, True)
-picam.capture('{}_sl.jpg'.format(U_FILENAME), use_video_port=True)
+picam.capture('{}_sl.jpg'.format(U_FILENAME))
+
+# flash led
+GPIO.output(13, GPIO.HIGH)
+time.sleep(0.5)
+GPIO.output(13, GPIO.LOW)
+GPIO.cleanup()
