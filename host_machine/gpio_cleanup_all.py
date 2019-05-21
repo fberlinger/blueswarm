25939b7812ffa9a2a99f6c9512daa72de53da1c8
Melvin Gauci <mgauci@g.harvard.edu>
2019-05-21 10:51:08 -0400

    Initial commit of host side files

---
+++ host_machine/gpio_cleanup_all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+base_command = "gpio_cleanup"
+
+for i in range(101, 110):
+    command = "ssh -o ConnectTimeout=5 pi@192.168.0." + str(i) + " '" + base_command + "' > /dev/null 2>&1 &"
+    # print(command)
+    subprocess.call(command, shell=True)
