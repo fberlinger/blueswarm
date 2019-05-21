16ff5cfaf3382ca32b9a5bda0466a64126dd3d48
Melvin Gauci <mgauci@g.harvard.edu>
2019-05-21 10:50:10 -0400

    Delete old files

--- host_machine_code/ssh_multiple.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-base_command = "flash_leds"
-
-for i in range(101, 110):
-    command = "ssh -o ConnectTimeout=5 pi@192.168.0." + str(i) + " '" + base_command + "' &"
-    print(command)
-    subprocess.call(command, shell=True)
